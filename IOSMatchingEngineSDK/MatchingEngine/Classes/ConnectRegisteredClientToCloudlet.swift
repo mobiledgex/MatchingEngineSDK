1b22077cb81e7456b1b803b4bc5406ac0022b1db
franklin-huang-mobiledgex <franklin.huang@mobiledgex.com>
2019-11-10 23:48:58 -0800

    remove some comments and print statements

--- IOSMatchingEngineSDK/MatchingEngine/Classes/ConnectRegisteredClientToCloudlet.swift
+++ IOSMatchingEngineSDK/MatchingEngine/Classes/ConnectRegisteredClientToCloudlet.swift
@@ -69,7 +69,6 @@ extension MatchingEngine {
         }
     }
     
-    // Developer will not be able to implement callbacks....
     public func registerAndFindTCPConnection(devName: String, appName: String, appVers: String, carrierName: String, location: [String: Any]) -> Promise<CFSocket>
     {
         let registerClientRequest = self.createRegisterClientRequest(devName: devName,
@@ -114,7 +113,6 @@ extension MatchingEngine {
         }
     }
     
-    // RegisterClient -> FindCloudlet -> GetTCPConnection (returns Promise<CFSocket>)
     @available(iOS 12.0, *)
     public func registerAndFindTCPTLSConnection(devName: String, appName: String, appVers: String, carrierName: String, location: [String: Any]) -> Promise<NWConnection>
     {
@@ -214,7 +212,6 @@ extension MatchingEngine {
     // RegisterClient -> FindCloudlet -> GetUDPConnection (returns Promise<CFSocket>)
     public func registerAndFindUDPConnection(devName: String, appName: String, appVers: String, carrierName: String, location: [String: Any]) -> Promise<CFSocket>
     {
-        /*let promiseInputs: Promise<URLSessionStreamTask> = Promise<URLSessionStreamTask>.pending()*/
         let registerClientRequest = self.createRegisterClientRequest(devName: devName,
                                                                      appName: appName,
                                                                      appVers: appVers,
@@ -349,32 +346,6 @@ extension MatchingEngine {
                 return promiseInputs
             }
             return self.getHTTPConnection(host: serverFqdn, port: port)
-        }.catch { error in
-            return error
-        }
-    }
-    
-    // DME host and port are specified (returns Promise<URLRequest>)
-    public func registerAndFindHTTPConnection(host: String, port: UInt, devName: String, appName: String, appVers: String, carrierName: String, location: [String: Any]) -> Promise<URLRequest>
-    {
-        let registerClientRequest = self.createRegisterClientRequest(devName: devName,
-                                                                     appName: appName,
-                                                                     appVers: appVers,
-                                                                     carrierName: carrierName,
-                                                                     authToken: nil)
-        return self.registerClient(host: host, port: port, request: registerClientRequest)
-        .then { (registerClientReply) -> Promise<[String: AnyObject]> in
-            print("RegisterClientReply is \(registerClientReply)")
-            
-            let findCloudletRequest = self.createFindCloudletRequest(carrierName: carrierName,
-                                                                     gpsLocation: location,
-                                                                     devName: devName,
-                                                                     appName: appName,
-                                                                     appVers: appVers)
-            return self.findCloudlet(host: host, port: port, request: findCloudletRequest)
-        }.then { findCloudletReply in
-            print("FindCloudletReply is \(findCloudletReply)")
-            return self.getHTTPConnection(host: host, port: String(port))
         }.catch { error in
             return error
         }
