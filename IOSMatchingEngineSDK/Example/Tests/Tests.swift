718b46758add125c6751744b0cecf2271a53fc92
franklin-huang-mobiledgex <52431019+franklin-huang-mobiledgex@users.noreply.github.com>
2020-08-14 14:09:34 -0700

    Endport fix (#86)
    * validate desired port, fix end port range
    
    * bump to 2.1.3
    
    * update readme
    
    * nit print statement

--- IOSMatchingEngineSDK/Example/Tests/Tests.swift
+++ IOSMatchingEngineSDK/Example/Tests/Tests.swift
@@ -77,7 +77,6 @@ class Tests: XCTestCase {
     
     func testRegisterClient() {
         let request = matchingEngine.createRegisterClientRequest(orgName: orgName, appName: appName, appVers: appVers)
-
         // Host goes to mexdemo, not gddt. gddt is the registered name for the app.
         var replyPromise: Promise<MobiledgeXiOSLibrary.MatchingEngine.RegisterClientReply>!
         
