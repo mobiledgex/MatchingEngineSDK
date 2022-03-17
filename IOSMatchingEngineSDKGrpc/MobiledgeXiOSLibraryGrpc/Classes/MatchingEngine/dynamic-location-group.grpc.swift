d1b7c993f75334cc9a3aca24de2df2e4b94ee329
Garner Lee <1672745+lgarner@users.noreply.github.com>
2022-03-17 16:06:47 -0700

    Add @_implementationOnly to framework grpc code.

--- IOSMatchingEngineSDKGrpc/MobiledgeXiOSLibraryGrpc/Classes/MatchingEngine/dynamic-location-group.grpc.swift
+++ IOSMatchingEngineSDKGrpc/MobiledgeXiOSLibraryGrpc/Classes/MatchingEngine/dynamic-location-group.grpc.swift
@@ -20,8 +20,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-import GRPC
-import NIO
+@_implementationOnly import GRPC
+@_implementationOnly import NIO
 import SwiftProtobuf
 
 
